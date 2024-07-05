a3ee0753c45f2806eb42bf9d2923bf484db9dcfe
HKL2003 <130469604+HKL2003@users.noreply.github.com>
2024-07-05 20:45:42 +0800

    Barrier2

--- Assets/Script/Barrier/Barrier.cs
+++ Assets/Script/Barrier/Barrier.cs
@@ -9,18 +9,18 @@ public class Barrier : MonoBehaviour
     public int speed;
     //障碍物实际速度
     public int actualSpeed;
-    //障碍物重力
-    public int gravity;
     //障碍物移动方向
     public int horizontalDirection;//左右
     public int verticalDirection;//上下
     //移动时间
     public int moveTime;
+    public int isImpaired;
     public int impairment=1;
     private Rigidbody2D rb;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isImpaired = moveTime;
     }
     private void OnEnable()
     {
@@ -39,17 +39,30 @@ public class Barrier : MonoBehaviour
             if(this.horizontalDirection == 0)
             {
                 if(verticalDirection == 1)
+                {
                     this.transform.Translate(Vector3.up * speed * Time.deltaTime);
+                    subtraction();
+                }   
                 else
+                {
                     this.transform.Translate(Vector3.down * speed * Time.deltaTime);
+                    subtraction();
+                }        
             }
             else
             {
                 if (horizontalDirection == 1)
-                    this.transform.Translate(Vector3.left*speed*Time.deltaTime);
+                {
+                    this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+                    subtraction();
+                }
                 else
+                {
                     this.transform.Translate(Vector3.right * speed * Time.deltaTime);
+                    subtraction();
+                }
             }
+            
         }
     }
     private void OnStateSwitching()
@@ -57,10 +70,21 @@ public class Barrier : MonoBehaviour
         if(speed == 0)
         {
             speed = actualSpeed;
-            this.m
+            this.impairment = 1;
         }else
         {
             speed = 0;
+            this.impairment = 0;
+        }
+    }
+
+    private void subtraction()
+    {
+        isImpaired = (int)(moveTime - impairment * Time.deltaTime);
+        if (isImpaired == 0)
+        {
+            verticalDirection = -1;
+            isImpaired = moveTime;
         }
     }
 }
