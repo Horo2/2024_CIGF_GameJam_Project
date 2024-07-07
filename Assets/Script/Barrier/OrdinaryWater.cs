75f992edb6226ab5394d6903a08b3ec42e98b111
HKL2003 <130469604+HKL2003@users.noreply.github.com>
2024-07-07 09:51:29 +0800

    new

--- Assets/Script/Barrier/OrdinaryWater.cs
+++ Assets/Script/Barrier/OrdinaryWater.cs
@@ -7,11 +7,14 @@ public class OrdinaryWater : MonoBehaviour
     public BoxCollider2D waterCollider;
     public BoxCollider2D waterTrigger;
     private Animator animator;
+    public float offset;
+    public float y;
     // Start is called before the first frame update
     void Start()
     {
         waterCollider = this.GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
+        y = transform.position.y;
     }
 
     // Update is called once per frame
@@ -30,6 +33,29 @@ public class OrdinaryWater : MonoBehaviour
     }
     //当时间停止时，有碰撞体积，玩家无法穿过。当时间流动时，没有碰撞体积，玩家可以穿过。
     //如果玩家在水里停止时间，则直接死亡。
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!PlayerController.GetisDisable())
+        {
+            if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "InteractObject")
+            {
+                this.transform.position = new Vector3(transform.position.x, y - offset, transform.position.z);
+            }
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!PlayerController.GetisDisable())
+        {
+            if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "InteractObject")
+            {
+                this.transform.position = new Vector3(transform.position.x, y, transform.position.z);
+            }
+        }
+    }
 
 
 }
