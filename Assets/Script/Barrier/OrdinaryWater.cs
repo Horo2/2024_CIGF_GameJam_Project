0647c905c27bcfe2f271d75ca7e1090a335fb878
Horo <49852694+Horo2@users.noreply.github.com>
2024-07-07 09:48:41 +0800

    关卡更新

--- Assets/Script/Barrier/OrdinaryWater.cs
+++ Assets/Script/Barrier/OrdinaryWater.cs
@@ -31,9 +31,5 @@ public class OrdinaryWater : MonoBehaviour
     //当时间停止时，有碰撞体积，玩家无法穿过。当时间流动时，没有碰撞体积，玩家可以穿过。
     //如果玩家在水里停止时间，则直接死亡。
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (!PlayerController.GetisDisable() && collision.gameObject.CompareTag("Player") == true)
-            PlayerController.Instance.Restrat();
-    }
+
 }
